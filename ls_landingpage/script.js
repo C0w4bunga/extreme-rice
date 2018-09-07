e939498376c07c0c8829b8c00ad36ea610c9abb5
Cowabunga <37673407+C0w4bunga@users.noreply.github.com>
2018-09-07 15:19:48 +0200

    initial commit

---
+++ ls_landingpage/script.js
@@ -0,0 +1,17 @@
+$('#clock').countdown('2018/10/05 21:00:00')
+.on('update.countdown', function(event) {
+  console.log(event);
+  var format = '%H:%M:%S';
+  if(event.offset.totalDays > 0) {
+    format = '%-d day%!d ' + format;
+  }
+  if(event.offset.weeks > 0) {
+    format = '%-w week%!w ' + format;
+  }
+  $(this).html(event.strftime(format));
+})
+.on('finish.countdown', function(event) {
+  $(this).html('This offer has expired!')
+    .parent().addClass('disabled');
+
+});
